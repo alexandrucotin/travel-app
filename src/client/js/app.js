c8b5e481dc2825cf557056fe3e5844911d8fd51e
Alexandru Cotin <a.cotin96@gmail.com>
2020-05-08 20:09:36 +0200

    retrieve lat and lng from geonames api

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,11 +1,40 @@
-function submit() {
+(function () {
   const button = document.getElementById("submitCity");
   button.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("ZAOOOO");
+    const city = document.getElementById("inputCity").value;
+    const country = document.getElementById("inputCountry").value;
+    getDataFromApi(city, country).then((data) => {
+      const element = {
+        lat: data.geonames[0].lat,
+        lng: data.geonames[0].lng,
+      };
+      postData("http://localhost:8081/data", element);
+    });
   });
-}
+})();
 
-window.submit = submit;
+//get data from api
+const getDataFromApi = async (city, country) => {
+  const url = `http://api.geonames.org/searchJSON?name_equals=${city}&country=${country}&maxRows=1&username=a.cotin96`;
+  const res = await fetch(url);
+  //changed to celcius (im from Italy)
+  try {
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log("error", err);
+  }
+};
 
-export { submit };
+const postData = async (url = "", data = {}) => {
+    console.log("data that IS in postData: ", data);
+    const res = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
--- src/server/server.js
+++ src/server/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-//express 
+//express
 const app = express();
+projectData = {};
 
 //middlewares
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("dist"));
@@ -15,10 +16,19 @@ console.log(__dirname);
 
 //routes
 
-app.get('/', (req, res) =>{
-    res.sendFiles("dist/index.html");
-})
+app.get("/", (req, res) => {
+  res.sendFiles("dist/index.html");
+});
 
+app.post("/data", function (req, res) {
+  const {lat, lng} = req.body;
+  projectData = {
+    lat,
+    lng
+  };
+  console.log(projectData)
+  res.status(202).send();
+});
 
 //starting the server
 const port = 8081;
