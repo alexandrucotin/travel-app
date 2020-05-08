6833f944fd0761e4b83b67c5d350be63892c576f
Alexandru Cotin <a.cotin96@gmail.com>
2020-05-08 11:31:55 +0200

    first commit for the structure of the app

---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  module: "development",
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  stats: "verbose",
+  output: {
+    library: "Client",
+    libraryTarget: "var",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "src/client/style"), "node_modules"],
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "src/client/style"), "node_modules"],
+  },
+  output: {
+    library: "Client",
+    libraryTarget: "var",
+  },
+};
