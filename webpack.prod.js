03e01367432e4fcc0a65d1ceafd2ceb5730170e7
Alexandru Cotin <a.cotin96@gmail.com>
2020-05-19 18:11:08 +0200

    Added service worker tests and fixed webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,18 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
+   optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   module: {
     rules: [
       {
@@ -14,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
 
       {
@@ -32,6 +40,19 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: false,
+      protectWebpackAssets: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new WorkboxPlugin.GenerateSW()
   ],
   resolve: {
     modules: [path.resolve(__dirname, "src/client/style"), "node_modules"],
